443116394c5afb7ff6a0f6b98ca729de472147cb
Benedict <jokanolaadedipupo@gmail.com>
2021-09-04 22:51:55 +0100

    add new music

--- controller/musicController.js
+++ controller/musicController.js
@@ -16,7 +16,7 @@ exports.addNewMusic = async (req, res) => {
     try {
         let music = await Music.find()
         
-        return res.status(200).json(music);
+        
     } catch (error) {
         res.status(500).json(error);
     }
